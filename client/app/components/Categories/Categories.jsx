e0d33594ae716b84af1223dc2a473ee4559ad51f
Ajay Tanwar <ajay_kumar@creativefrenzy.in>
2026-02-12 11:05:49 +0530

    fiexed design of shop by categories

--- client/app/components/Categories/Categories.jsx
+++ client/app/components/Categories/Categories.jsx
@@ -13,15 +13,15 @@ export default function Categories() {
         <h2 className="text-2xl md:text-3xl font-bold mb-8">
           Shop by Categories
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
+        <div className="grid grid-cols-3  sm:grid-cols-3 gap-8">
           {categories.map((cat, index) => (
             <a
               key={index}
               href={cat.link}
               className="flex flex-col items-center p-6 bg-white shadow-lg rounded-2xl hover:shadow-xl transition"
             >
-              <div className="text-5xl text-[#59abab] mb-4">{cat.icon}</div>
-              <h3 className="text-lg font-semibold">{cat.name}</h3>
+              <div className=" text-3xl sm:text-5xl text-[#59abab] mb-4">{cat.icon}</div>
+              <h3 className=" text-xs sm:text-lg font-semibold">{cat.name}</h3>
             </a>
           ))}
         </div>
